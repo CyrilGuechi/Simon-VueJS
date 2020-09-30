c748a888270a98a1b3dbed4f6b9494a884107fed
CyrilGuechi <cyrilguechi71@gmail.com>
2020-09-30 18:12:21 +0200

    Create sequence

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
-import Vue from 'vue'
-import './style/style.css';
+const vm = new Vue({
+    el: '#app',
+    data: {
+        topLeft: false,
+        topRight: false,
+        botLeft: false,
+        botRight: false,
+        tmp: [],
+        sequence: [],
+        squareMap: ['topLeft','topRight','botLeft','botRight']
+    },
+    methods: {
+        addNewElToSequence(){
+           this.sequence.push(this.squareMap[Math.floor(Math.random() * 4)]);
+        },
+        newGame() {
+            this.addNewElToSequence();
+        }
+    }
+})
